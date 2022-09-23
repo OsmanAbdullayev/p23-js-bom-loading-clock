f97f0270eba539dc7f6fbb1bde5c2a1319bd351e
Osman <os.abdullayev@gmail.com>
2022-09-23 21:52:29 +0400

    After finishing the first version

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+let loading = document.querySelector(".loading");
+let clock = document.querySelector(".clock");
+let pClock = document.querySelector(".pClock");
+let pLoading = document.querySelector(".pLoading");
+
+// Functions
+function loadingEnds() {
+	loading.style.display = "none";
+	clock.style.display = "flex";
+}
+
+function updatingLoading() {
+	let seconds = new Date().getSeconds();
+
+	if (seconds % 2 === 0) {
+		pLoading.innerHTML = "Loading..";
+	} else if (seconds % 3 === 0) {
+		pLoading.innerHTML = "Loading...";
+	} else {
+		pLoading.innerHTML = "Loading.";
+	}
+}
+
+function updatingClock() {
+	let hours = new Date().getHours();
+	let minutes = new Date().getMinutes();
+	let seconds = new Date().getSeconds();
+	pClock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+// Timeout and Interval
+setInterval(updatingLoading, 1000);
+setInterval(updatingClock, 1000);
+setTimeout(loadingEnds, 4000);
