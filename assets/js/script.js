40c6244660bb3f6e633a10eee5e996e6088e84d5
Osman <os.abdullayev@gmail.com>
2022-09-24 15:52:48 +0400

    after fixing the dot updating algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,48 @@ let loading = document.querySelector(".loading");
 let clock = document.querySelector(".clock");
 let pClock = document.querySelector(".pClock");
 let pLoading = document.querySelector(".pLoading");
+loading.style.display = "flex";
+clock.style.display = "none";
+pLoading.innerHTML = "Loading."; // this will run immediately
 
 // Functions
+function initialLoading() {
+	pLoading.innerHTML = "Loading.."; // this will run after 0.5 seconds
+	setTimeout(() => {
+		pLoading.innerHTML = "Loading..."; // this will run after 1 second
+	}, 500);
+}
+
+if (loading.style.display == "flex") {
+	function updatingLoading() {
+		pLoading.innerHTML = "Loading."; // this will run after 1.5 seconds
+		setTimeout(() => {
+			pLoading.innerHTML = "Loading.."; // this will run after 2 seconds
+			setTimeout(() => {
+				pLoading.innerHTML = "Loading..."; //this will run after 2.5 seconds
+			}, 500);
+		}, 500);
+	}
+}
+
+
+
 function loadingEnds() {
 	loading.style.display = "none";
 	clock.style.display = "flex";
-}
-
-function updatingLoading() {
-	let seconds = new Date().getSeconds();
-
-	if (seconds % 2 === 0) {
-		pLoading.innerHTML = "Loading..";
-	} else if (seconds % 3 === 0) {
-		pLoading.innerHTML = "Loading...";
-	} else {
-		pLoading.innerHTML = "Loading.";
-	}
 }
 
 function updatingClock() {
 	let hours = new Date().getHours();
 	let minutes = new Date().getMinutes();
 	let seconds = new Date().getSeconds();
-	pClock.innerHTML = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+	pClock.innerHTML = `${String(hours).padStart(2, "0")}:${String(
+		minutes
+	).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 }
 
 // Timeout and Interval
-setInterval(updatingLoading, 1000);
+setTimeout(initialLoading, 500);
+setInterval(updatingLoading, 1500);
 setInterval(updatingClock, 1000);
-setTimeout(loadingEnds, 4000);
+setTimeout(loadingEnds, 3000);
